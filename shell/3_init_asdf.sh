464a8d8b8aa261cbf3dc7aca5b9c9a65cf5aef21
Sharanan Kulam <sharu95@users.noreply.github.com>
2024-11-27 07:18:16 +0100

    refactor: asdf setup

---
+++ shell/3_init_asdf.sh
@@ -0,0 +1,17 @@
+# Add plugins
+asdf plugin add python
+asdf plugin add nodejs
+asdf plugin add terraform
+asdf plugin add terraform-docs
+
+# Install latest
+asdf install python latest
+asdf install nodejs latest
+asdf install terraform latest
+asdf install terraform-docs latest
+
+# Set globals
+asdf global python latest
+asdf global nodejs latest
+asdf global terraform latest
+asdf global terraform-docs latest
--- shell/init.sh
+++ shell/init.sh
@@ -1,5 +1,6 @@
 source 1_init_install.sh
 source 2_init_brew_essentials.sh # Install init_brew.sh if customs are needed
-source 3_init_env.sh
-source 4_init_shell.sh
+source 3_init_asdf.sh
+source 4_init_env.sh
+source 5_init_shell.sh
 echo "Installed all shell related things. use init_brew.sh to install additionals, now only essentials are installed"
