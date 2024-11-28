df5aa9d37ab8b057edf7d19e0e960929cdad12a2
Sharanan Kulam <sharu95@users.noreply.github.com>
2024-11-28 10:48:08 +0100

    update pdm

--- shell/3_init_asdf.sh
+++ shell/3_init_asdf.sh
@@ -3,15 +3,18 @@ asdf plugin add python
 asdf plugin add nodejs
 asdf plugin add terraform
 asdf plugin add terraform-docs
+asdf plugin add pdm
 
 # Install latest
 asdf install python latest
 asdf install nodejs latest
 asdf install terraform latest
 asdf install terraform-docs latest
+asdf install pdm latest
 
 # Set globals
 asdf global python latest
 asdf global nodejs latest
 asdf global terraform latest
 asdf global terraform-docs latest
+asdf global pdm latest
