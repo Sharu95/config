21634948ffef87fec08d8bae72b75829103e9bb0
Sharanan Kulam <sharu95@users.noreply.github.com>
2024-12-05 11:13:26 +0100

    add auto change py env

--- shell/1_init_install.sh
+++ shell/1_init_install.sh
@@ -27,6 +27,7 @@ done
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autoswitch_virtualenv
 
 source "$HOME/.zshrc"
 
