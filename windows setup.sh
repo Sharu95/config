a107f1f62b7b8e445291004a115b2d95abef64a7
Sharanan Kulam <sharu95@users.noreply.github.com>
2025-09-03 16:00:53 +0200

    Add some draft for windows clients

---
+++ windows setup.sh
@@ -0,0 +1,13 @@
+# Install WSL
+winget install Microsoft.VisualStudioCode
+
+wsl --install
+
+# In WSL:
+sudo apt update 
+sudo apt install zsh
+sudo apt install build-essential
+
+
+# Download firacode
+https://github.com/ryanoasis/nerd-fonts/releases/
