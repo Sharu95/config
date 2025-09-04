23006a983742b41882b20d7566057cb1c51ac781
Sharanan Kulam <sharu95@users.noreply.github.com>
2025-09-04 08:48:10 +0200

    Update script for WSL setup for vs code

--- vscode/init.sh
+++ vscode/init.sh
@@ -11,12 +11,19 @@ copy_settings() {
 		VS_SETTINGS="$HOME/.config/Code/User"
 	elif [[ $1 == "mac" ]]; then
 		VS_SETTINGS="$HOME/Library/Application Support/Code/User"
+	elif [[ $1 == "win" ]]; then  
+		# WSL path 
+		VS_SETTINGS="$HOME/.vscode-server/data/User"
 	else 
-		echo "$fg_bold[red]| Specify system for settings path <mac | linux>$reset_color"
+		echo "$fg_bold[red]| Specify system for settings path <mac | linux | win>$reset_color"
 		exit
 	fi
 	cp settings.json $VS_SETTINGS/settings.json
-	cp keybindings.json $VS_SETTINGS/keybindings.json
+	if [[ $1 == "win" ]]; then
+		cp keybindings_win.json $VS_SETTINGS/keybindings.json
+	else
+		cp keybindings.json $VS_SETTINGS/keybindings.json
+	fi
 }
 
 echo "$fg_bold[yellow]| Installing extensions$reset_color"
--- windows setup.sh
+++ windows setup.sh
@@ -1,13 +1,19 @@
 # Install WSL
-winget install Microsoft.VisualStudioCode
-
 wsl --install
+
+# Windows applications
+winget install Microsoft.VisualStudioCode
+winget install httpie
+code --install-extension ms-vscode-remote.remote-wsl # Install WSL extension in VS code to do WSL things like settings etc (in Win env)
 
 # In WSL:
 sudo apt update 
 sudo apt install zsh
+sudo apt install lsd
+sudo apt install httpie
 sudo apt install build-essential
-
+sudo apt install lzma unzip
+sudo apt install jq
 
 # Download firacode
 https://github.com/ryanoasis/nerd-fonts/releases/
